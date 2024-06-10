9d998f8d41471d70c898de00e5f9342beef8f777
NIkio <114503729+MRLQ@users.noreply.github.com>
2024-06-10 19:54:27 +0800

    Update IconCollection.vue

--- src/views/icon/IconCollection.vue
+++ src/views/icon/IconCollection.vue
@@ -130,6 +130,10 @@ const defaultIconCollectionColumns = ref([
     text: "Orz-3/Color+",
     value: "https://raw.githubusercontent.com/Orz-3/mini/master/Color%2B.json",
   },
+  {
+    text: "MRLQ/ios_rule_script",
+    value: "https://raw.githubusercontent.com/MRLQ/ios_rule_script/master/icons.json",
+  },
 ]);
 
 // 默认图标仓库
